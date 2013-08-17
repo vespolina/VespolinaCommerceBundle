7aa0ff3caccbb00368cc1ad032043967ed417314
inspiran <daniel@xerias.be>
2013-08-17 14:18:35 +0200

    Made get_price() less error prone

--- Twig/Extension/CommerceExtension.php
+++ Twig/Extension/CommerceExtension.php
@@ -11,56 +11,39 @@ namespace Vespolina\CommerceBundle\Twig\Extension;
 
 use Vespolina\Pricing\Manager\PricingManager;
 use Vespolina\Pricing\Manager\PricingManagerInterface;
-use Vespolina\StoreBundle\Resolver\StoreResolverInterface;
 
 class CommerceExtension extends \Twig_Extension
 {
     protected $assetManager;
     protected $pricingManager;
-    protected $storeResolver;
 
-    public function __construct(AssetManager $assetManager,
-                                PricingManagerInterface $pricingManager,
-                                StoreResolverInterface $storeResolver)
+    public function __construct(AssetManager $assetManager = null, PricingManagerInterface $pricingManager = null)
     {
         $this->assetManager = $assetManager;
         $this->pricingManager = $pricingManager;
-        $this->storeResolver = $storeResolver;
     }
 
-    /**
-     * Return a list of supported Twig functions
-     *
-     * @return array
-     */
     public function getFunctions()
     {
         return array(
+            'assetManager' => new \Twig_Function_Method($this, 'getAssetManager'),
             'price' => new \Twig_Function_Method($this, 'getPrice')
         );
     }
 
-    /**
-     * Retrieve the price by the name of the pricing element for the given entity
-     *
-     * @param $entity
-     * @param $priceElementName
-     * @param string $currency
-     * @return string
-     */
+    public function getAssetManager()
+    {
+        return $this->assetManager;
+    }
+
     public function getPrice($entity, $priceElementName, $currency = '')
     {
-        if (null == $entity) return;
-
-        $pricingSet = $entity->getPricing();
-
-        if (null == $pricingSet) return;
-
+        if (null == $entity->getPricing()) return;
         $pricingSet = $this->pricingManager->process($entity->getPricing());
         $pricingValue = $pricingSet[$priceElementName];
 
         if (null == $pricingValue) return;
-
+        
         $cur = $currency != '' ? $currency :  $pricingValue->getCurrency();
 
         return $this->priceFormat(
@@ -68,33 +51,11 @@ class CommerceExtension extends \Twig_Extension
                 $cur);
     }
 
-    /**
-     * Retrieve the name of the extension
-     *
-     * @return string
-     */
     public function getName()
     {
         return 'commerce_bundle_extension';
     }
 
-    /**
-     * Get the active store
-     *
-     * @return \Vespolina\StoreBundle\Model\StoreInterface
-     */
-    public function getStore()
-    {
-        return $this->storeResolver->getStore();
-    }
-
-    /**
-     * Format the given amount and optionally currency
-     *
-     * @param $amount
-     * @param null $currency
-     * @return string
-     */
     protected function priceFormat($amount, $currency = null) {
 
         $left = '';
